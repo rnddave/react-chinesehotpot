5f9f71694fc20797c6c9a58b0dbcd87eb3073c9d
David Dickinson <rnddave@gmail.com>
2023-11-22 16:28:01 +0000

    some styling work

--- online-store/src/routes/shop/shop.component.jsx
+++ online-store/src/routes/shop/shop.component.jsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router-dom';
 import { useContext } from 'react';
 import { ProductsContext } from '../../contexts/products.context';
 import ProductCard from '../../components/product-card/product-card.component';
+import './shop.styles.scss'
 
 const Shop = () => {
 
         const {products} = useContext(ProductsContext);
 
     return (
-        <div>
+        <div className='products-container'>
             {products.map((product) => (
             
                 <ProductCard key={product.id} product={product}/>
