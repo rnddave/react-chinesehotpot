a0463bb475129d656a05de5a7ede0d8c9c00f8a9
David Dickinson <rnddave@gmail.com>
2023-11-22 10:39:14 +0000

    revisiting the userContext (Reasct Context) material

--- online-store/src/components/sign-in-form/sign-in-form.component.jsx
+++ online-store/src/components/sign-in-form/sign-in-form.component.jsx
@@ -3,6 +3,7 @@ import { useState, useContext } from 'react';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
+// we want the sign-in-form to access user context (to know if anyone signed in basically)
 import { UserContext } from '../../contexts/user.context';
 
 import {
--- online-store/src/contexts/user.context.jsx
+++ online-store/src/contexts/user.context.jsx
@@ -1,18 +1,50 @@
 import { createContext, useState } from "react";
 
-// value we want to access
+// the 'ACTUAL' value we want to access
 export const UserContext = createContext({
+    // context needs a value as well
+    // this should be a null value for the empty state (an empty object still evaluate as true)
     currentUser: null,
-    setCurrentUser: () => null,
 
+    // what is the default value? 
+    // we just want to return null, so if no user, then set state to null
+    setCurrentUser: () => null,
 });
 
+// provider is like a wrapper around the components that need access to this state
 export const UserProvider = ({ children }) => {
+    
+    // current user stores the actual value and the setter function (initialised as null)
     const [currentUser, setCurrentUser] = useState(null);
+    // but now we want to pass the important VALUE's (current user and the set() current user)
     const value = { currentUser, setCurrentUser};
 
-
-
+    // any children can access values inside the useState() 
     return <UserContext.Provider value={value}>{ children }</UserContext.Provider>
-}
+};
 
+
+/*
+So this userProvider needs to be wrapped around whatever components need access to it. We can do this from the index.js
+
+currently:
+
+  <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </React.StrictMode>
+
+change it to 
+
+  <React.StrictMode>
+    <BrowserRouter>
+    
+        // this is the new bit !!
+        <UserProvider>
+            <App />
+        </UserProvider>
+
+    </BrowserRouter>
+  </React.StrictMode>
+*/
