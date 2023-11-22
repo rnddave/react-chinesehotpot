bad45b74b74c73ab0c521a45b26faea9b0c8600e
David Dickinson <rnddave@gmail.com>
2023-11-22 16:22:38 +0000

    created a product context and a product card

--- online-store/src/routes/shop/shop.component.jsx
+++ online-store/src/routes/shop/shop.component.jsx
@@ -1,7 +1,20 @@
 import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import { ProductsContext } from '../../contexts/products.context';
+import ProductCard from '../../components/product-card/product-card.component';
 
 const Shop = () => {
-        return <h1>I am the shop page (HoLdEr PaGe) </h1>;
+
+        const {products} = useContext(ProductsContext);
+
+    return (
+        <div>
+            {products.map((product) => (
+            
+                <ProductCard key={product.id} product={product}/>
+            ))}
+        </div>
+        );
     };
     <Outlet />
 
