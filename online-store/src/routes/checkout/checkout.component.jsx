66b03af3569d85e53939a11c6387cc5079375ff0
David Dickinson <rnddave@gmail.com>
2023-11-30 10:37:32 +0000

    made a checkout-item and associated styling, moving all cartItem calls to this new component and then styled checkout page instrucing it to call the new checkout item - however, it looks like ZtM has a slightly different path so I am writing this before checking out their code example

--- online-store/src/routes/checkout/checkout.component.jsx
+++ online-store/src/routes/checkout/checkout.component.jsx
@@ -1,29 +1,33 @@
-import { useContext } from 'react';
-
-import { CartContext } from '../../contexts/cart-context';
-
 import './checkout.styles.scss'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 
 const Checkout = () => {
-    const { cartItems, addItemToCart, removeItemFromCart, removeLineItemFromCart } = useContext(CartContext);
+    
 
     return (
-        <div className='checkout-items-container'>
-            <div className='img'> 
-                {cartItems.map((cartItem) => {
-                    const {id, name, quantity} = cartItem
-                    return <div key={id}>
-                            <h2>{name}</h2>
-                            <span>{quantity}</span>
-                            <span onClick={() => removeItemFromCart(cartItem)}> decrease</span>
-                            <br />
-                            <span onClick={() => addItemToCart(cartItem)}> increase</span>
-                            <br />
-                            <span onClick={() => removeLineItemFromCart(cartItem)}> remove</span>
-                        </div>
-                    })}
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
+            <CheckoutItem />
+            <div className='total'>
+                    <span>Total: 0</span>
+                </div>
         </div>
     );
 }
