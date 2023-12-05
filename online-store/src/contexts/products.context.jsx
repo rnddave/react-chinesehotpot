5ab3378aa64b93d605cb99ced3e85293eeb086cb
David Dickinson <rnddave@gmail.com>
2023-12-05 13:51:01 +0000

    working through adding things to and reading from the firestore db

--- online-store/src/contexts/products.context.jsx
+++ online-store/src/contexts/products.context.jsx
@@ -1,13 +1,33 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+// don't need this line after we've done the initial set data to db. 
+// import SHOP_DATA from '../shop-data.js';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+  // we're using this use-effect to build the initial database entries, 
+  // but after running, we should remove otherwise we will re-initialise the database 
+  // each time it is run - which probably isn't that useful... 
+  /* 
+  useEffect(() => {
+    addCollectionAndDocuments('caegories', SHOP_DATA);
+  }, []);
+  */
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments('caegories'); // NB I mistyped Categories when creating it in DB
+      console.log(categoryMap)
+    }
+
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
