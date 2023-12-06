4163fe7f2ccc92fa7a2e158e2b2d6dff34048d09
David Dickinson <rnddave@gmail.com>
2023-12-06 14:14:33 +0000

    migrated to Styled Components = product-card

--- online-store/src/components/product-card/product-card.component.jsx
+++ online-store/src/components/product-card/product-card.component.jsx
@@ -3,7 +3,12 @@ import { CartContext } from '../../contexts/cart-context';
 
 import Button from '../button/button.component';
 
-import './product-card.styles.scss'
+import {
+    ProductCartContainer,
+    Footer,
+    Name,
+    Price,
+  } from './product-card.styles';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
@@ -12,14 +17,14 @@ const ProductCard = ({ product }) => {
     const addProductToCart = () => addItemToCart(product);
 
     return (
-        <div className='product-card-container'>
+        <ProductCartContainer>
             <img src={imageUrl} alt={`${name}`} />
-            <div className='footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>£{price}</span>
-            </div>
+            <Footer>
+                <Name as='span'>{name}</Name>
+                <Price as='span'>£{price}</Price>
+            </Footer>
             <Button buttonType='inverted' onClick={addProductToCart}>Add to cart</Button>
-        </div>
+        </ProductCartContainer>
     )
 }
 
