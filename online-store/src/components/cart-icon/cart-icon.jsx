9edef005f4c2acf2926eec4a9540db0cfe29e00a
David Dickinson <rnddave@gmail.com>
2023-12-06 13:44:10 +0000

    migrated to styled components = cart-icon

--- online-store/src/components/cart-icon/cart-icon.jsx
+++ online-store/src/components/cart-icon/cart-icon.jsx
@@ -5,7 +5,11 @@ import { CartContext } from '../../contexts/cart-context';
 import { ReactComponent as Basket } from '../../assets/shopping-bag.svg'
 
 
-import './cart-icon.styles.scss'
+import {
+    CartIconContainer,
+    CartIconImage,
+    CartItemCount
+} from './cart-icon.styles'
 
 const CartIcon = () => {
 
@@ -13,10 +17,12 @@ const CartIcon = () => {
     const toggleIsCartOpen = () => setCartShow(!cartShow);
 
     return(
-        <div className='cart-icon-container'>
-            <Basket className='shopping-icon' onClick={toggleIsCartOpen} />
-            <span className='item-count'>{itemsInCart}</span>
-        </div>
+        <CartIconContainer onClick={toggleIsCartOpen}>
+            <Basket >
+                <CartIconImage />
+            </Basket>
+            <CartItemCount as='span'>{itemsInCart}</CartItemCount>
+        </CartIconContainer>
     )
 };
 
---
+++ online-store/src/components/cart-icon/cart-icon.styles.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const CartIconContainer = styled.div`
+  width: 45px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export const CartIconImage = styled.div`
+  width: 24px;
+  height: 24px;
+`;
+
+export const CartItemCount = styled.div`
+  position: absolute;
+  font-size: 10px;
+  font-weight: bold;
+  bottom: 12px;
+`;
+
+// cart-icon.styles.scss 
+/* .cart-icon-container {
+  width: 45px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  .shopping-icon {
+    width: 24px;
+    height: 24px;
+  }
+
+  .item-count {
+    position: absolute;
+    font-size: 10px;
+    font-weight: bold;
+    bottom: 12px;
+  }
+} */
